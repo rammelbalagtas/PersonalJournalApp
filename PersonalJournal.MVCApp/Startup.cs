a890a54bff81d29cb0139c97b0e1b4931eea6ae6
Rammel <rammel.balagtas@gmail.com>
2022-03-23 16:13:34 -0400

    Modify database name for the identity information
    Modify the database name for the asp identity tables. Apply migration and create the databse.

--- PersonalJournal.MVCApp/Startup.cs
+++ PersonalJournal.MVCApp/Startup.cs
@@ -29,7 +29,7 @@ namespace PersonalJournal.MVCApp
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("IdentityDB")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
