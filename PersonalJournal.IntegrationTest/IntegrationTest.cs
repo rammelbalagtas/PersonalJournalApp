7a8a49b4bdcd9f385c3f9a8e99ac942d463ebfb1
Rammel <rammel.balagtas@gmail.com>
2022-03-25 20:41:12 -0400

    Add integration test for Category pages
    Add browser test for navigating to Categories page and for adding a new category

--- PersonalJournal.IntegrationTest/IntegrationTest.cs
+++ PersonalJournal.IntegrationTest/IntegrationTest.cs
@@ -40,6 +40,41 @@ namespace PersonalJournal.IntegrationTest
             Assert.IsTrue(_webDriver.Title.Contains("Register"));
         }
 
+        [TestMethod]
+        public void Test_Title_Categories()
+        {
+            simulateLogin();
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Categories/");
+            Assert.IsTrue(_webDriver.Title.Contains("Categories"));
+        }
+
+        [TestMethod]
+        public void Test_Add_Category()
+        {
+            simulateLogin();
+            //navigate to add new categories and submit
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Categories/Create/");
+            var titleInput = _webDriver.FindElement(By.Id("Title"));
+            titleInput.SendKeys("Category Title");
+            var longDescriptionInput = _webDriver.FindElement(By.Id("LongDescription"));
+            longDescriptionInput.SendKeys("Category Description");
+            var submit = _webDriver.FindElement(By.Id("Btn_Create"));
+            submit.Click();
+            Assert.IsTrue(true);
+        }
+
+        private void simulateLogin()
+        {
+            //log in to the page first
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Login/");
+            var emailAddressInput = _webDriver.FindElement(By.Id("Input_Email"));
+            emailAddressInput.SendKeys("test9@gmail.com");
+            var passwordInput = _webDriver.FindElement(By.Id("Input_Password"));
+            passwordInput.SendKeys("Test@1234");
+            var login = _webDriver.FindElement(By.Id("login-submit"));
+            login.Click();
+        }
+
         [TestCleanup]
         public void Teardown()
         {
