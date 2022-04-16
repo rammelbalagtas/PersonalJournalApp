e448816f955d36ae3b3997575291cfc8dc568468
Rammel <rammel.balagtas@gmail.com>
2022-04-15 20:19:48 -0400

    Fix the issue with speech recognition
    Instead of just displaying the text when it's final, display the interim transcript results for a smoother experience.

--- PersonalJournal.MVCApp/wwwroot/js/SpeechToTextRecognition.js
+++ PersonalJournal.MVCApp/wwwroot/js/SpeechToTextRecognition.js
@@ -17,19 +17,51 @@ else {
     var micImage2 = document.getElementById('mic2-img');
 
     var recognizing;
+    var isInterim;
+    var temp;
 
     var recognizer = new window.SpeechRecognition();
     recognizer.continuous = true;
+    recognizer.interimResults = true;
     reset();
 
     recognizer.onresult = function (event) {
 
+        // old code
         //var subsectionText = document.activeElement;
+        //for (var i = event.resultIndex; i < event.results.length; ++i) {
+        //    if (event.results[i].isFinal) {
+        //        subsectionText1.value += event.results[i][0].transcript;
+        //    }
+        //}
+
+        var interim_transcript = "";
+
+        if (!isInterim) {
+            temp = subsectionText1.value; //transfer current value to a temp variable
+            isInterim = true //start interim recognition
+        }
+
+        //once recognition is final, move back the previous value and append the final transcript
+        if (event.results[event.resultIndex].isFinal) {
+            isInterim = false;
+            subsectionText1.value = temp;
+        }
+
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
                 subsectionText1.value += event.results[i][0].transcript;
+            } else {
+                 interim_transcript += event.results[i][0].transcript;
             }
         }
+
+        //while interim recognition is running, keep replacing the subtext value with 
+        //interim transcript
+        if (isInterim) {
+            subsectionText1.value = temp + interim_transcript;
+        }
+
     }
 
     subsection1Btn.addEventListener('click', function () {
@@ -38,7 +70,6 @@ else {
             recognizer.stop();
             reset();
         } else {
-            /* recognizer.interimResults = true;*/
             recognizer.start();
             recognizing = true;
             micImage1.src = "../mic-animate.gif";
