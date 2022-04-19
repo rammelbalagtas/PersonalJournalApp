c68b06b8c474fc3c999f2f6ba56339cb211b6c3d
Rammel <rammel.balagtas@gmail.com>
2022-04-19 12:05:38 -0400

    Add the speech to text functionality for the subsection text and description in journal entries page

--- PersonalJournal.MVCApp/wwwroot/js/SpeechToTextRecognition.js
+++ PersonalJournal.MVCApp/wwwroot/js/SpeechToTextRecognition.js
@@ -8,77 +8,213 @@ if (window.SpeechRecognition === null) {
 
 else {
 
+    var descriptionText = document.getElementById('Description');
+    var descriptionBtn = document.getElementById('description-text-btn');
+    var micDescription = document.getElementById('mic-desc');
+
     var subsectionText1 = document.getElementById('SubsectionText1');
     var subsection1Btn = document.getElementById('subsection1-text-btn');
-    var micImage1 = document.getElementById('mic1-img');
+    var micSub1 = document.getElementById('mic-sub1');
 
     var subsectionText2 = document.getElementById('SubsectionText2');
     var subsection2Btn = document.getElementById('subsection2-text-btn');
-    var micImage2 = document.getElementById('mic2-img');
+    var micSub2 = document.getElementById('mic-sub2');
+
+    var subsectionText3 = document.getElementById('SubsectionText3');
+    var subsection3Btn = document.getElementById('subsection3-text-btn');
+    var micSub3 = document.getElementById('mic-sub3');
+
+    var subsectionText4 = document.getElementById('SubsectionText4');
+    var subsection4Btn = document.getElementById('subsection4-text-btn');
+    var micSub4 = document.getElementById('mic-sub4');
+
+    var subsectionText5 = document.getElementById('SubsectionText5');
+    var subsection5Btn = document.getElementById('subsection5-text-btn');
+    var micSub5 = document.getElementById('mic-sub5');
 
     var recognizing;
     var isInterim;
-    var temp;
+    var textHolder;
+    var elementId;
 
     var recognizer = new window.SpeechRecognition();
     recognizer.continuous = true;
     recognizer.interimResults = true;
-    reset();
+    recognizing = false;
 
     recognizer.onresult = function (event) {
 
-        // old code
-        //var subsectionText = document.activeElement;
-        //for (var i = event.resultIndex; i < event.results.length; ++i) {
-        //    if (event.results[i].isFinal) {
-        //        subsectionText1.value += event.results[i][0].transcript;
-        //    }
-        //}
+        switch (elementId) {
+            case "description-text-btn":
+                handleOnResultSpeech(descriptionText);
+                break;
+            case "subsection1-text-btn":
+                handleOnResultSpeech(subsectionText1);
+                break;
+            case "subsection2-text-btn":
+                handleOnResultSpeech(subsectionText2);
+                break;
+            case "subsection3-text-btn":
+                handleOnResultSpeech(subsectionText3);
+                break;
+            case "subsection4-text-btn":
+                handleOnResultSpeech(subsectionText4);
+                break;
+            case "subsection5-text-btn":
+                handleOnResultSpeech(subsectionText5);
+                break;
+        }
+
+    }
+
+    //attach event listeners
+    descriptionBtn.addEventListener('click', onMicButtonClick);
+    subsection1Btn.addEventListener('click', onMicButtonClick);
+    subsection2Btn.addEventListener('click', onMicButtonClick);
+    subsection3Btn.addEventListener('click', onMicButtonClick);
+    subsection4Btn.addEventListener('click', onMicButtonClick);
+    subsection5Btn.addEventListener('click', onMicButtonClick);
+
+    function handleOnResultSpeech(subsectionText) {
 
         var interim_transcript = "";
 
         if (!isInterim) {
-            temp = subsectionText1.value; //transfer current value to a temp variable
+            textHolder = subsectionText.value + " "; //transfer current value to a temp variable
             isInterim = true //start interim recognition
         }
 
         //once recognition is final, move back the previous value and append the final transcript
         if (event.results[event.resultIndex].isFinal) {
             isInterim = false;
-            subsectionText1.value = temp;
+            subsectionText.value = textHolder;
         }
 
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
-                subsectionText1.value += event.results[i][0].transcript;
+                subsectionText.value += event.results[i][0].transcript;
             } else {
-                 interim_transcript += event.results[i][0].transcript;
+                interim_transcript += event.results[i][0].transcript;
             }
         }
 
         //while interim recognition is running, keep replacing the subtext value with 
         //interim transcript
         if (isInterim) {
-            subsectionText1.value = temp + interim_transcript;
+            subsectionText.value = textHolder + interim_transcript;
         }
-
     }
 
-    subsection1Btn.addEventListener('click', function () {
-        //toggle between start and stop
+    //event handler method for onclick of mic buttons
+    function onMicButtonClick() {
+        elementId = document.activeElement.id;
         if (recognizing) {
             recognizer.stop();
-            reset();
+            recognizing = false;
+            switch(elementId) {
+                case "description-text-btn":
+                    micDescription.src = "../mic.gif";
+                    micSub1.hidden = false;
+                    micSub2.hidden = false;
+                    micSub3.hidden = false;
+                    micSub4.hidden = false;
+                    micSub5.hidden = false;
+                    break;
+                case "subsection1-text-btn":
+                    micSub1.src = "../mic.gif";
+                    micDescription.hidden = false;
+                    micSub2.hidden = false;
+                    micSub3.hidden = false;
+                    micSub4.hidden = false;
+                    micSub5.hidden = false;
+                    break;
+                case "subsection2-text-btn":
+                    micSub2.src = "../mic.gif";
+                    micDescription.hidden = false;
+                    micSub1.hidden = false;
+                    micSub3.hidden = false;
+                    micSub4.hidden = false;
+                    micSub5.hidden = false;
+                    break;
+                case "subsection3-text-btn":
+                    micSub3.src = "../mic.gif";
+                    micDescription.hidden = false;
+                    micSub1.hidden = false;
+                    micSub2.hidden = false;
+                    micSub4.hidden = false;
+                    micSub5.hidden = false;
+                    break;
+                case "subsection4-text-btn":
+                    micSub4.src = "../mic.gif";
+                    micDescription.hidden = false;
+                    micSub1.hidden = false;
+                    micSub2.hidden = false;
+                    micSub3.hidden = false;
+                    micSub5.hidden = false;
+                    break;
+                case "subsection5-text-btn":
+                    micSub5.src = "../mic.gif";
+                    micDescription.hidden = false;
+                    micSub1.hidden = false;
+                    micSub2.hidden = false;
+                    micSub3.hidden = false;
+                    micSub4.hidden = false;
+                    break;
+            }
         } else {
             recognizer.start();
             recognizing = true;
-            micImage1.src = "../mic-animate.gif";
+            switch(elementId) {
+                case "description-text-btn":
+                    micDescription.src = "../mic-animate.gif";
+                    micSub1.hidden = true;
+                    micSub2.hidden = true;
+                    micSub3.hidden = true;
+                    micSub4.hidden = true;
+                    micSub5.hidden = true;
+                    break;
+                case "subsection1-text-btn":
+                    micSub1.src = "../mic-animate.gif";
+                    micDescription.hidden = true;
+                    micSub2.hidden = true;
+                    micSub3.hidden = true;
+                    micSub4.hidden = true;
+                    micSub5.hidden = true;
+                    break;
+                case "subsection2-text-btn":
+                    micSub2.src = "../mic-animate.gif";
+                    micDescription.hidden = true;
+                    micSub1.hidden = true;
+                    micSub3.hidden = true;
+                    micSub4.hidden = true;
+                    micSub5.hidden = true;
+                    break;
+                case "subsection3-text-btn":
+                    micSub3.src = "../mic-animate.gif";
+                    micDescription.hidden = true;
+                    micSub1.hidden = true;
+                    micSub2.hidden = true;
+                    micSub4.hidden = true;
+                    micSub5.hidden = true;
+                    break;
+                case "subsection4-text-btn":
+                    micSub4.src = "../mic-animate.gif";
+                    micDescription.hidden = true;
+                    micSub1.hidden = true;
+                    micSub2.hidden = true;
+                    micSub3.hidden = true;
+                    micSub5.hidden = true;
+                    break;
+                case "subsection5-text-btn":
+                    micSub5.src = "../mic-animate.gif";
+                    micDescription.hidden = true;
+                    micSub1.hidden = true;
+                    micSub2.hidden = true;
+                    micSub3.hidden = true;
+                    micSub4.hidden = true;
+                    break;
+            }
         }
-    });
-
-    function reset() {
-        recognizing = false;
-        micImage1.src = "../mic.gif";
     }
 
 }
